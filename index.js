de72705202785c50e0afc7c9373bd4d92d8b4322
gracethedragon <gracethedragon@hotmail.com>
2022-03-10 21:45:47 -0600

    added function

--- index.js
+++ index.js
@@ -7,6 +7,48 @@ import methodOverride from 'method-override'
 import moment from 'moment';
 
 
+//function to calculate drink overall abv
+export const calcStrength = (amount_arr, abv_arr) =>{
+  const amount = amount_arr.map(amt_arr=> parseFloat(amt_arr))
+  
+  console.log(amount, abv_arr)
+  let alcContent = 0
+  amount.forEach((amt,index)=> {
+    alcContent += amt * abv_arr[index]
+    // console.log(amount,abv_arr[index], amount*abv_arr[index], alcContent)
+  })
+  const totalVol = amount.reduce((a,b)=> a+b)
+  const drinkAbv = alcContent / totalVol * 100
+  console.log(totalVol, alcContent, drinkAbv)
+  return drinkAbv
+}
+// calcStrength(amount_arr, abv_arr)
+
+// const amount_arr = [2, .25, 1, .25]
+// const flavor_arr = ["alcohol", "fruity", "fruity", "sweet"]
+
+//function to calculate drink overall flavour type
+export const flavorList = (amount_arr, flavor_arr)=>{
+  const amount = amount_arr.map(amt_arr=> parseFloat(amt_arr))
+  const flavObj = {sweet: 0, sour: 0, bitter:0, savory:0, spicy:0, fruity:0, overall_abv:0}
+  const totalVol = amount.reduce((a,b)=> a+b)
+  const amount_arrPerc = amount.map(amt=>(amt/totalVol))
+  // console.log(amount_arrPerc)
+  flavor_arr.forEach((flavor, index)=>{
+  if(flavor!=="alcohol"){
+    if (Object.keys(flavObj).includes(flavor)){
+        flavObj[flavor] += amount_arrPerc[index]
+      } else {
+        flavObj[flavor] = amount_arrPerc[index]
+      }
+    }
+  })
+    
+  // console.log(flavObj)
+  return flavObj
+}
+
+
 const PORT = process.env.PORT || 3004;
 moment().format();
 const {Pool} = pg
